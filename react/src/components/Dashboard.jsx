42648b860f2ce6e2a7c3ec56295840f07808d150
Mac <elmeftouhi@gmail.com>
2023-01-13 16:58:26 +0100

    dashboard component

---
+++ react/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from '../assets/images'
+
+function Dashboard() {
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+        <img src='{{img}}'></img>
+    </div>
+  )
+}
+
+export default Dashboard
